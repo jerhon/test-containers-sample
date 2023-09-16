1c62a10669bf28ec0f0cf132283e45979f717017
Jeremy Honl <jerhon@users.noreply.github.com>
2023-09-16 13:04:42 -0500

    Add initial code.

---
+++ hs-test-containers-sample/DbScripts/init.sql
@@ -0,0 +1,19 @@
+﻿CREATE USER starfleet;
+CREATE DATABASE starfleet;
+
+GRANT ALL PRIVILEGES ON DATABASE starfleet TO starfleet;
+GRANT ALL PRIVILEGES ON DATABASE starfleet TO postgres;
+
+\connect starfleet
+
+CREATE TABLE CrewMembers
+    (Id INT PRIMARY KEY,
+    Name VARCHAR(30));
+
+INSERT INTO CrewMembers 
+    VALUES
+    (0, 'Patrick Picard'),
+    (1, 'William Riker'),
+    (2, 'Geordi Laforge'),
+    (3, 'Data'),
+    (4, 'Beverly Crusher');
---
+++ hs-test-containers-sample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Reflection;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using Npgsql;
+
+// Important: Init scripts are copied to the bin directory when compiled.
+var initScriptsPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+initScriptsPath = Path.Combine(initScriptsPath, "DbScripts");
+
+var password = Guid.NewGuid().ToString();
+var container = new ContainerBuilder()
+    .WithImage("postgres:16")
+    .WithPortBinding(5432, true)
+    .WithBindMount(initScriptsPath, "/docker-entrypoint-initdb.d", AccessMode.ReadOnly)
+    .WithEnvironment("POSTGRES_PASSWORD", password)
+    .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5432))
+    .Build();
+
+try {
+    await container.StartAsync();
+}
+finally {
+ 
+    var (stdout, stderr) = await container.GetLogsAsync();
+
+    Console.WriteLine(stdout);   
+}
+
+var connectionString = $"Host=localhost;Port={container.GetMappedPublicPort(5432)};Username=postgres;Password={password};Database=starfleet";
+using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+await connection.OpenAsync();
+
+using var crewMembersCommand = connection.CreateCommand();
+crewMembersCommand.CommandText = "SELECT * FROM CrewMembers";
+
+using var crewMembersReader = await crewMembersCommand.ExecuteReaderAsync();
+
+while (await crewMembersReader.ReadAsync()) {
+    string name = crewMembersReader.GetFieldValue<string>("Name");
+    Console.WriteLine(name);
+}
+
